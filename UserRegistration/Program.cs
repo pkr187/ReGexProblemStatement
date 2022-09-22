e0e1a3b8954f2e3fca7bda24b2959a2c5e498f6d
pkr187 <pankajroman187@gmail.com>
2022-09-22 22:35:52 +0530

    [pankaj] Add:FirstNameValidation

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.FirstName("Pkr");
+        }
+    }
+}
---
+++ UserRegistration/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class User
+    {
+        //UC 1 Checkig for First Name Validation
+        public const string FIRSTNAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public void FirstName(string firstname)
+        {
+            Regex regex = new Regex(FIRSTNAME_REGEX);
+            bool result = regex.IsMatch(firstname);
+            Console.WriteLine("UC-1: Entered First Name is " + result);
+        }
+    }
+}
+    
+
